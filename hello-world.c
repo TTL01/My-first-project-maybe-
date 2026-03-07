ed1acf600ef96c9be1dcafa3cbe41132a61ab54d
Time To Live <humip@proton.me>
2026-03-07 08:36:11 +0000

    New

---
+++ hello-world.c
@@ -0,0 +1,8 @@
+//not that much because i still learn
+#include <stdio.h>
+int main(){
+ 
+ printf("Hello World");
+ puts("\n What else are you waiting for?");
+ return 0;
+}
---
+++ hello-world.py
@@ -0,0 +1,9 @@
+#idk but that easy to much 
+#i swear who is in my mind better than this but akhh
+user = input("how You can sepelit 'Hello world' ?: ").lower()
+ if user = "hello world":
+    print("congratulations Honey (:")
+else:
+    print("Get out Honey of Shit")
+
+#in Future i'll change it 
